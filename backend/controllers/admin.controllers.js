cf94f00e1016aaaecc84908bfe6ba842247b262a
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-01 23:45:42 +0300

    finished the create course function

--- backend/controllers/admin.controllers.js
+++ backend/controllers/admin.controllers.js
@@ -3,6 +3,7 @@ const Course = require("../models/courseModel")
 exports.createCourse = async (req, res) => {
     const { name } = req.body
 
+    //creating course
     const course = new Course()
     course.name = name
     await course.save()

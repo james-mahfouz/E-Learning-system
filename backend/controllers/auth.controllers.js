d0168326bef356868cbcb6bf3d0a0a8bd34444b3
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-01 20:54:35 +0300

    adjusted the register function and now fully functional

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -2,17 +2,23 @@ const User = require("../models/userModel")
 const jwt = require("jsonwebtoken")
 
 exports.register = async (req, res) => {
-    const { name, email, password } = req.body;
+    const { name, email, password, role } = req.body;
 
     const existingUser = await User.findOne({ email })
 
     if (existingUser) return res.status(409).json({ message: "Email already exists" })
 
+
     const user = new User();
+    if (role) user.role = role;
     user.name = name;
     user.email = email;
     user.password = password;
 
     await user.save();
-    const { password: hashedPassword, ...newUser } = user.toJSON
+    const { password: hashedPassword, ...newUser } = user.toJSON()
+    res.status(201).json(newUser)
 }
+
+
+
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
 const router = Router();
 
-const { login, register } = require("../controllers/auth.contollers")
-
-router.get("/login", login);
+const { register } = require("../controllers/auth.controllers")
+// login,
+// router.get("/login", login);
 router.post("/register", register);
 
 module.exports = router;

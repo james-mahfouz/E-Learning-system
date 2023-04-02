817b1288c0ffe5e77b6b4b32ac4041256e76d4e0
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-02 08:43:54 +0300

    finished the enroll function in user controller

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,3 +1,24 @@
-exports.enroll = () => {
-    console.log('hello world')
+const Course = require("../models/courseModel")
+const User = require("../models/userModel")
+
+exports.enroll = async (req, res) => {
+    try {
+        const courseId = req.params.courseId;
+        const userId = req.body._id
+
+        const course = await Course.findByIdAndUpdate(courseId, {
+            $push: { students: userId }
+        })
+
+        const user = await User.findByIdAndUpdate(userId, {
+            $push: { courses: courseId }
+        })
+
+        res.status(200).json({
+            message: `User ${userId} enrolled in course ${courseId}`,
+            data: { course, user }
+        })
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
 }

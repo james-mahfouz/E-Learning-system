176da813dd8c72e5bdbc4167c093a0d822be42c1
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-02 09:31:41 +0300

    finished the api to get all the courses with every student enrolled in it

--- backend/controllers/admin.controllers.js
+++ backend/controllers/admin.controllers.js
@@ -9,3 +9,19 @@ exports.createCourse = async (req, res) => {
 
     res.json(course)
 }
+
+
+exports.get_users_courses = async (req, res) => {
+    try {
+        const courses = await Course.find().populate({
+            path: 'students',
+            select: '-role -password -courses'
+        });
+        res.status(200).json({
+            message: "Users fetched successfully",
+            data: { courses }
+        })
+    } catch (e) {
+        res.status(500).json({ error: e.message })
+    }
+}
--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express")
 
-const { createCourse } = require("../controllers/admin.controllers")
+const { createCourse, get_users_courses } = require("../controllers/admin.controllers")
 const router = Router()
 
 router.post("/create_course", createCourse)
+router.get("/get_users_courses", get_users_courses)
 
 module.exports = router
